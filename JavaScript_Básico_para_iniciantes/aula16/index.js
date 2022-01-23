0fac955ecfd7e4754f179d0960a3376ad2585e6f
Lucian Rafael Montibeller <luciandeborah@gmail.com>
2022-01-22 23:04:01 -0300

    aula feitaw

---
+++ JavaScript_Básico_para_iniciantes/aula16/index.js
@@ -0,0 +1,30 @@
+const alunos = ['Lucian', 'Debora', 'Rafael'];
+console.log(alunos);
+
+
+
+//alunos[alunos.length] = 'Luiza'
+//alunos[alunos.length] = 'joão'
+//alunos[alunos.length] = 'pedro'
+
+
+const removido = alunos.shift()
+console.log(removido)
+console.log(alunos)
+
+console.log(alunos.slice(0,2))
+
+
+
+//const removido = alunos.pop()
+//console.log(removido)
+
+//console.log(alunos);
+
+
+
+//alunos.pop()
+//alunos.unshift('ANA')
+//console.log(alunos[0]);
+//console.log(alunos[2]);
+
