b9f27692efc12f36adda0b3f7b22af81ab3b7e70
Lucian Rafael Montibeller <luciandeborah@gmail.com>
2022-01-22 21:53:25 -0300

    alterando nome da pasta

---
+++ JavaScript_Básico_para_iniciantes/aula15/index.js
@@ -0,0 +1,7 @@
+let num = 9.43242342;
+console.log(Math.floor(num));
+console.log(Math.ceil(num));
+console.log(Math.round(num));
+console.log(Math.max(1,2,3,4,5));
+console.log(Math.min(1,2,3,4,5))
+console.log(Math.round(Math.random() * (10-5) + 5));
