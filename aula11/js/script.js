924ecc0875d44142dde1e0f7d084c670d708a018
Lucian Rafael Montibeller <luciandeborah@gmail.com>
2022-01-22 21:32:44 -0300

    feita as aulas

---
+++ aula11/js/script.js
@@ -0,0 +1,1 @@
+alert('Conteúdo da mensagem!')
---
+++ aula20_operadores_arit_atrib_increm/index.js
@@ -0,0 +1,13 @@
+/*
+ *Aritméticos
+ * + Adição / concatenação
+ * - / *  
+*/
+
+//const num1 = 2;
+//const num2 = 10;
+//console.log(num1 * num2)
+
+let contador = 1;
+contador ++;
+console.log(contador)
