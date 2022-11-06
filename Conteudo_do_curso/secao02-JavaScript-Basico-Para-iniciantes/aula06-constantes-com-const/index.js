1bdca2f9e290963f05818f74f9641fad6565e218
LucianMontibeller <luciandeborah@gmail.com>
2022-11-06 13:13:00 -0300

    aula06

---
+++ Conteudo_do_curso/secao02-JavaScript-Basico-Para-iniciantes/aula06-constantes-com-const/index.js
@@ -0,0 +1,18 @@
+// não podemos criar constantes com palavras reservadas
+// constantes precisam ter nomes significativos
+// não pode começar o nome de uma constante com um número
+// não podem conter espaços ou traços
+// utilizamos cameCase
+// não pode modificar o valor de uma constante
+// não utilize VAR ,UTILIZE CONST.
+
+const nome = 'Lucian';
+console.log(nome);
+
+
+const primeiroNumero = 5;
+const segundoNumero = 10;
+const resultado = primeiroNumero * segundoNumero;
+console.log(resultado)
+
+
