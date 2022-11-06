c14ca2e039cd805543f6f15e46d1363ed6066c8f
LucianMontibeller <luciandeborah@gmail.com>
2022-11-06 14:14:24 -0300

    aula09

---
+++ Conteudo_do_curso/secao02-JavaScript-Basico-Para-iniciantes/aula09-tipos-de-dados-primitivos/index.js
@@ -0,0 +1,8 @@
+const nome = 'Lucian'; // String
+const num1 = 10; // number
+const num2 = 10.5 // number
+let nomeAluno; // undefined = não aponta pra local algum na memória
+const sobrenomeAluno = null; // Nulo -> não aponta  pra local algum na memória
+const aprovado = true; // boolean = true ou false (lógico)
+
+console.log(typeof nome, num1)
