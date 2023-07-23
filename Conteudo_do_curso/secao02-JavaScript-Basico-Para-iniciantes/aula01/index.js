c95ed788d029c87e25edca0cc90b93ded5a4768f
Lucian <luciandeborah@gmail.com>
2023-07-23 09:15:43 -0300

    inicial

--- Conteudo_do_curso/secao02-JavaScript-Basico-Para-iniciantes/aula01/index.js
+++ Conteudo_do_curso/secao02-JavaScript-Basico-Para-iniciantes/aula01/index.js
@@ -1,5 +1,6 @@
 console.log('Lucian Rafael'); // String =Texto
 console.log('Lucian Rafael');
 console.log('Lucian', "Rafael");
+console.log('Lucian', "Rafael");
 
 console.log(123456, 15.85, 'Lucian Rafael');
