1eefc1bbfeeba8077fe8408bc33b527c155da6c1
Lucian <luciandeborah@gmail.com>
2023-07-23 10:24:14 -0300

    aula10

---
+++ Conteudo_do_curso/secao02-JavaScript-Basico-Para-iniciantes/aula10-operadores-aritmeticos-de-atribuicao-incremento/index.js
@@ -0,0 +1,48 @@
+/*
+ * Aritméticos
+ *
+ * + Adição/Concatenação
+ * ** Pontenciação
+ * % Resto da divisão
+ *
+ * */
+
+
+const num1 = 5;
+const num2 = 2;
+console.log(num1 + num2)
+console.log(num1 ** num2)
+console.log(num1 % num2)
+console.log(num1 - num2)
+
+let contador = 1;
+++contador;
+++contador;
+++contador;
+++contador;
+console.log(contador)
+
+
+
+let conta = 1;
+conta += 5;
+console.log(conta);
+conta += 5;
+console.log(conta);
+conta += 5;
+console.log(conta);
+conta += 5;
+console.log(conta);
+
+// NaN not a number
+// parseInt (inteiro) parseFloat(ponto flutuante) Number(número)
+
+const n1 = 5;
+const n2 = parseInt('5');
+console.log(n1 + n2);
+const n3 = 5;
+const n4 = parseFloat('5.2');
+console.log(n3 + n4);
+const n5 = 5;
+const n6 = Number('5');
+console.log(n5 + n6);
