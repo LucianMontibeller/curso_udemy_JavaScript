ce97ba3fba848db6dd608d115d254197b7f84594
Lucian <luciandeborah@gmail.com>
2023-07-27 21:07:54 -0300

    aula16

---
+++ secao02-JavaScript-Basico-Para-iniciantes/aula16-arrays-basico/index.js
@@ -0,0 +1,21 @@
+//                0        1        2
+const alunos = ['Luiz', 'Maria', 'joão'];
+
+alunos[0] = 'eduardo';
+alunos[3] = 'Luiza';
+
+//const removido = alunos.pop(shift);
+//alunos.push('Lucian');
+//alunos.unshift('Lucian');
+
+//console.log(alunos.length);
+//console.log(alunos[0]);
+//console.log(alunos[2]);
+//delete alunos[1]
+//console.log(alunos.slice(0, 3))
+
+console.log(typeof alunos);
+console.log(alunos instanceof Array);
+console.log(alunos);
+
+
